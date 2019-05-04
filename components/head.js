aaf947559d65110607b57b991cee42604d2a7ecc
Benjamin Murphy <ben@benjaminmurphy.me>
2019-05-04 03:04:29 +0000

    Add final blog post.

---
+++ components/head.js
@@ -0,0 +1,62 @@
+import Head from 'next/head';
+
+export default () => (
+  <Head>
+    <title>benjamin murphy</title>
+
+    <link
+      rel='stylesheet'
+      href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css'
+      integrity='sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm'
+      crossOrigin='anonymous' />
+
+    <link rel='stylesheet' href='https://use.typekit.net/zyj0uow.css' />
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Cormorant+Garamond:400,400i|Raleway:200,200i' />
+
+    <style jsx global>{`
+      .niveau {
+        font-family: 'niveau-grotesk-small-caps', sans-serif;
+      }
+
+      .eaves {
+        font-family: 'mrs-eaves-roman-lining', serif;
+      }
+
+      .cormorant {
+        font-family: 'Cormorant Garamond', serif;
+      }
+
+      .raleway {
+        font-family: 'Raleway', sans-serif;
+      }
+
+      .isabelline {
+        color: #efefef;
+      }
+
+      .cerulean {
+        color: #0e4177;
+      }
+
+      .red {
+        color: #de1a1a;
+      }
+
+      .charcoal {
+        color: #2c4251;
+      }
+
+      .jet {
+        color: #363537;
+      }
+
+      html, body, div#__next, div#base {
+        height: 100%;
+      }
+
+      a {
+        color: #0e4177;
+      }
+    `}</style>
+  </Head>
+);
--- pages/project.js
+++ pages/project.js
@@ -47,6 +47,13 @@ export default () => (
         font-size: 0.8em;
         margin-bottom: 16px;
       }
+
+      img {
+        display: block;
+        width: 400px;
+        margin-left: auto;
+        margin-right: auto;
+      }
     `}</style>
 
     <Head />
@@ -60,6 +67,109 @@ export default () => (
         <p className='raleway charcoal credits'>
           A project created by Claire Kim, <a href='/'>Ben Murphy</a>, Heila Precel, Jared Samilow, and Katie Scholl for <a href='https://cs.brown.edu/courses/csci1951-a'>CSCI1951-A: Data Science.</a>
         </p>
+
+        <hr />
+
+        <div className='post'>
+          <p className='cormorant jet'>A Retrospective: Life Tenure Isn't Very Long.</p>
+          <p className='raleway charcoal date'>
+            May 3, 2019
+          </p>
+
+          <p className='raleway jet'>
+            Three months later, a successful poster presentation complete, and having run many lines of Python, we'd like to use this post to reflect
+            on what we've accomplished over the course of the semester, and what more is left to be done.
+          </p>
+          <h5 className='cormorant jet'>Overview</h5>
+          <p className='raleway jet'>
+            Our initial vision was to analyze polarization on the Supreme Court. We laid out a series of hypotheses related to this goal, and successfully
+            trained word2vec models on a series of corpora (CourtListener, then several corpora requested from <a href="case.law">case.law</a>. In our
+            <a href='ambulances.benjaminmurphy.me'>final interactive visualization</a>, you can experiment with this data yourself. However, we struggled
+            to draw meaningful insights from these models. Beyond top-words analyses and computing similarity scores between pairs of words, we didn't
+            find a holistic method of representing large-scale change over time.
+          </p>
+          <p className='raleway jet'>
+            So, we pivoted! We seem to do that frequently. Since our primary struggle was quantifying results from word2vec, we opted to look at a
+            fundamentally numeric metric: opinion length, and with this in mind we analyzed four hypotheses in our final work:
+          </p>
+          <ul className='raleway jet'>
+            <li>Hypothesis #1: Supreme Court opinions have increased in length between 1900 and today.</li>
+            <li>Hypothesis #2: As the number of opinions associated with a Supreme Court case increases, so too does the average length of each opinion.</li>
+            <li>Hypothesis #3: Supreme Court opinions are longer than the opinions of lower courts.</li>
+            <li>Hypothesis #4: Dissenting SCOTUS opinions are longer than majority opinions.</li>
+          </ul>
+          <p className='raleway jet'>
+            We found evidence to support the first three hypotheses, and no evidence to support the last. We'll discuss our conclusions at the end of
+            this post. We made significant progress towards analyzing opinion length, which we justified as a proxy for polarization on the Court, since
+            more contention generally produces longer arguments than highly consistent opinions. Hence, we consider our project to have produced meaningful
+            results, though there is of course always more to be done (and we'll discuss future directions later.)
+          </p>
+          <h5 className='cormorant jet'>Data</h5>
+          <p className='raleway jet'>
+            As previously mentioned, our final data set was sourced entirely from <a href='case.law'>case.law</a>, and included full text of all federal
+            court opinions (Supreme Court, circuit court, and district court), along with a host of metadata including decision date, opinion authors,
+            docket number, and more. We're confident the data set has many more insights contained within it, and we're interested in exploring more of
+            these in the future.
+          </p>
+          <p className='raleway jet'>
+            The data was already in good shape when we obtained it. It was in JSON format, and highly structured – besides slight noisiness in opinion
+            content (since some opinions were collected via optical character recognition), we didn't have anything to do besides insert it into a database,
+            which leads us to the next section of this post.
+          </p>
+          <h5 className='cormorant jet'>Methodology</h5>
+          <p className='raleway jet'>
+            We stored our data in a MongoDB instance running on MongoDB Atlas, their database-as-a-service offering. We explored the data using a number of
+            means, including <a href='https://www.mongodb.com/products/compass'>MongoDB Compass</a>, custom-built aggregation pipelines, and plenty of API
+            documentation.
+          </p>
+          <p className='raleway jet'>
+            For extracting insights from the data, we relied on prebuilt word2vec models provided via <a href='https://radimrehurek.com/gensim/'>gensim</a>,
+            which was invaluable in providing a clean interface for training our model. We cleaned the text of opinions using
+            <a href='https://www.nltk.org'>Python's nltk library</a>. For regressions, used to analyze opinion length, we used scipy's linregress function,
+            combined with a host of MongoDB calls to format the incoming data correctly, and subsequently visualized it using matplotlib.
+
+            For our web visualization, we had a complex stack. Our word2vec results were served via a Flask server that kept a word2vec model hot for online
+            querying. We ran a NodeJS web server for serving static files, complete with a full React stack involving Webpack, Babel, ReactJSS, and Bootstrap.
+          </p>
+          <h5 className='cormorant jet'>Conclusions and Future Directions</h5>
+          <p className='raleway jet'>
+            First, we'll discuss the results obtained, and the methods used for obtaining them.
+          </p>
+          <ul className='raleway jet'>
+            <li>
+              <h6 className='cormorant jet'>Hypothesis #1: Supreme Court opinions have increased in length between 1900 and today.</h6>
+              We found, using linear regression, an increase of approximately 700 words per opinion from 1900 to modern day. Our line of best fit had
+              an R-squared value of 0.405.
+              <img src='/static/decision_word_count_vs_year.png' />
+            </li>
+            <li>
+              <h6 className='cormorant jet'>Hypothesis #2: As the number of opinions associated with a Supreme Court case increases, so too does the average length of each opinion.</h6>
+              Opinion length followed a near-linear pattern, going from approximately 750 words for unanimous opinions to over 4000 words for decisions containing
+              eight opinions. Decisions with nine or ten opinions were the outliers, and hence were not well fit by our model, but given their relative scarcity
+              we do not believe this challenges the validity of our results. Our line of best fit yielded an R-squared value of 0.603.
+              <img src='/static/opinion_length_versus_unanimity.png' />
+            </li>
+            <li>
+              <h6 className='cormorant jet'>Hypothesis #3: Supreme Court opinions are longer than the opinions of lower courts.</h6>
+              We found that average opinion length across court lengths was highly variable, but that the Supreme Court retained a lead over circuit courts throughout
+              the time frame we considered. However, our hypothesis was incorrect when it comes to district courts: They retained a significant lead over both types of
+              appellate courts, and this lead appeared to be growing. However, this is likely attributable to the responsibility of district courts to summarize relevant
+              facts in the case, a responsibility not evenly shared by appellate courts.
+              <img src='/static/court_type.png' />
+            </li>
+            <li>
+              <h6 className='cormorant jet'>Hypothesis #4: Dissenting SCOTUS opinions are longer than majority opinions.</h6>
+              We found no noticeable difference between dissenting and majority opinion length at any point in the time frame we considered. This contradicts our expectation
+              that dissenting opinions would be longer. We hypothesized this originally because dissenting opinions are not controlling precedent, so any effort committed to
+              writing them is merely for posterity – and hence, flowery language and complex arguments shine.
+              <img src='/static/majority_v_dissent.png' />
+            </li>
+          </ul>
+          <p>
+            Our word2vec models are available <a href='ambulances.benjaminmurphy.me'>here</a>, and (upon inputting a query) yield a visualization of similarity, which
+            was produced by using React to generate svg elements in HTML.
+          </p>
+        </div>
 
         <hr />
 
